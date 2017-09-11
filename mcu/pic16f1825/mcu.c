fc96d49778811c63b4895d4ddbc7586eebed0032
Francois Berder <fberder@outlook.fr>
2017-09-11 21:17:46 +0100

    mcu: pic16f1825: Add makefile and mcu (incomplete) implementation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ mcu/pic16f1825/mcu.c
@@ -0,0 +1,44 @@
+/**
+ * This file is part of pic18-framework.
+ *
+ * pic18-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pic18-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with pic18-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mcu.h"
+
+
+void mcu_init(void)
+{
+    /* @todo Not implemented yet */
+}
+
+void mcu_delay(uint32_t nanoseconds)
+{
+    (void)nanoseconds;
+    /* @todo Not implemented yet */
+}
+
+void mcu_reset(void)
+{
+    asm("reset");
+}
+
+void mcu_idle(void)
+{
+    /* @todo Not implemented yet */
+}
+
+void mcu_sleep(void)
+{
+    /* @todo Not implemented yet */
+}
