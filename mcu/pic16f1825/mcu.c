19f7b892b27dfa41acf76f0d9044f276da65c9a1
Francois Berder <fberder@outlook.fr>
2017-09-12 22:36:48 +0100

    mcu: pic16f1825: Implement mcu init/register intr handler
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- mcu/pic16f1825/mcu.c
+++ mcu/pic16f1825/mcu.c
@@ -14,13 +14,49 @@
  * along with pic18-framework.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <xc.h>
+#include "board.h"
 #include "mcu.h"
 
+#ifndef MAX_INTERRUPT_COUNT
+#define MAX_INTERRUPT_COUNT     (8U)
+#endif
+
+static uint8_t intr_count;
+
+struct interrupt_entry_t {
+    uint8_t (*cond)(void);
+    void (*handler)(void*);
+    void *arg;
+};
+static struct interrupt_entry_t entries[MAX_INTERRUPT_COUNT];
+
+static void interrupt mcu_intr_handler(void)
+{
+    unsigned int i;
+    for (i = 0; i < intr_count; ++i) {
+        if (entries[i].cond())
+            entries[i].handler(entries[i].arg);
+    }
+}
 
 void mcu_init(void)
 {
-    /* @todo Not implemented yet */
+    /* Enable peripheral interrupts */
+    asm("bsf INTCON, 6");
+
+    mcu_enable_interrupts();
+}
+
+void mcu_register_intr_handler(void (*cond)(void), void(*handler)(void*), void *arg)
+{
+    assert(intr_count < MAX_INTERRUPT_COUNT);
+
+    entries[intr_count].cond = cond;
+    entries[intr_count].handler = handler;
+    entries[intr_count].arg = arg;
+    ++intr_count;
 }
 
 void mcu_delay(uint32_t nanoseconds)
