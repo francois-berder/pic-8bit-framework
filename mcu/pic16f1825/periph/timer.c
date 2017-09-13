e7cc1caf9606c6de095823c821f5fce73d795495
Francois Berder <fberder@outlook.fr>
2017-09-13 09:32:54 +0100

    mcu: pic16f1825: Implement timer interface
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ mcu/pic16f1825/periph/timer.c
@@ -0,0 +1,186 @@
+/**
+ * This file is part of pic18-framework.
+ *
+ * pic18-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pic18-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with pic18-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <xc.h>
+#include "mcu.h"
+#include "periph/timer.h"
+
+static uint32_t ticks[3];
+static uint8_t intr_registered[3];
+static void(*callbacks[3])(void);
+
+static uint8_t timer2_cond(void)
+{
+    return PIE1 & _PIE1_TMR2IE_MASK
+        && PIR1 & _PIR1_TMR2IF_MASK;
+}
+
+static uint8_t timer4_cond(void)
+{
+    return PIE3 & _PIE3_TMR4IE_MASK
+        && PIR3 & _PIR3_TMR4IF_MASK;
+}
+
+static uint8_t timer6_cond(void)
+{
+    return PIE3 & _PIE3_TMR6IE_MASK
+        && PIR3 & _PIR3_TMR6IF_MASK;
+}
+
+static void timer2_handler(void)
+{
+    ticks[0]++;
+    callbacks[0]();
+    PIR1 &= ~_PIR1_TMR2IF_MASK;
+}
+
+static void timer4_handler(void)
+{
+    ticks[1]++;
+    callbacks[1]();
+    PIR3 &= ~_PIR3_TMR4IF_MASK;
+}
+
+static void timer6_handler(void)
+{
+    ticks[2]++;
+    callbacks[2]();
+    PIR3 &= ~_PIR3_TMR6IF_MASK;
+}
+
+void timer_configure(uint8_t num,
+                     uint8_t prescaler, uint8_t postcaler,
+                     uint8_t period,
+                     void(*callback)(void))
+{
+    switch (num) {
+    case 2:
+        T2CON = prescaler & _T2CON_T2CKPS_MASK;
+        T2CON |= (postcaler << _T2CON_T2OUTPS_POSITION) & _T2CON_T2OUTPS_MASK;
+        PR2 = period;
+        callbacks[0] = callback;
+        break;
+    case 4:
+        T4CON = prescaler & _T4CON_T4CKPS_MASK;
+        T4CON |= (postcaler << _T4CON_T4OUTPS_POSITION) & _T4CON_T4OUTPS_MASK;
+        PR4 = period;
+        callbacks[1] = callback;
+        break;
+    case 6:
+        T6CON = prescaler & _T6CON_T6CKPS_MASK;
+        T6CON |= (postcaler << _T6CON_T6OUTPS_POSITION) & _T6CON_T6OUTPS_MASK;
+        PR6 = period;
+        callbacks[2] = callback;
+        break;
+    }
+
+    mcu_disable_interrupts();
+    switch (num) {
+    case 2:
+        if (intr_registered[0] == 0) {
+            intr_registered[0]++;
+            mcu_register_intr_handler(timer2_cond, timer2_handler, 0);
+        }
+        PIR1 &= ~_PIR1_TMR2IF_MASK;
+        PIE1 |= _PIE1_TMR2IE_MASK;
+        break;
+    case 4:
+        if (intr_registered[1] == 0) {
+            intr_registered[1]++;
+            mcu_register_intr_handler(timer4_cond, timer4_handler, 0);
+        }
+        PIR3 &= ~_PIR3_TMR4IF_MASK;
+        PIE3 |= _PIE3_TMR4IE_MASK;
+        break;
+    case 6:
+        if (intr_registered[2] == 0) {
+            intr_registered[2]++;
+            mcu_register_intr_handler(timer6_cond, timer6_handler, 0);
+        }
+        PIR3 &= ~_PIR3_TMR6IF_MASK;
+        PIE3 |= _PIE3_TMR6IE_MASK;
+        break;
+    }
+    mcu_enable_interrupts();
+}
+
+void timer_start(uint8_t num)
+{
+    switch (num) {
+    case 2:
+        T2CON |= _T2CON_TMR2ON_MASK;
+        break;
+    case 4:
+        T4CON |= _T4CON_TMR4ON_MASK;
+        break;
+    case 6:
+        T6CON |= _T6CON_TMR6ON_MASK;
+        break;
+    }
+}
+
+void timer_stop(uint8_t num)
+{
+    switch (num) {
+    case 2:
+        T2CON &= ~_T2CON_TMR2ON_MASK;
+        break;
+    case 4:
+        T4CON &= ~_T4CON_TMR4ON_MASK;
+        break;
+    case 6:
+        T6CON &= ~_T6CON_TMR6ON_MASK;
+        break;
+    }
+}
+
+uint32_t timer_get_tick_count(uint8_t num)
+{
+    uint32_t t;
+
+    mcu_disable_interrupts();
+    switch (num) {
+    case 2:
+        t = ticks[0];
+        break;
+    case 4:
+        t = ticks[1];
+        break;
+    case 6:
+        t = ticks[2];
+        break;
+    }
+    mcu_enable_interrupts();
+
+    return t;
+}
+
+void timer_reset_tick_count(uint8_t num)
+{
+    mcu_disable_interrupts();
+    switch (num) {
+    case 2:
+        ticks[0] = 0;
+        break;
+    case 4:
+        ticks[1] = 0;
+        break;
+    case 6:
+        ticks[2] = 0;
+        break;
+    }
+    mcu_enable_interrupts();
+}
