cd54a301e81f617e4a8ca1493590c70d578ae24d
Francois Berder <fberder@outlook.fr>
2017-09-14 20:13:48 +0100

    mcu: Define timer1 interface
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ mcu/include/periph/timer1.h
@@ -0,0 +1,40 @@
+/**
+ * This file is part of pic18-framework.
+ *
+ * pic18-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pic18-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with pic18-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __PERIPH_TIMER1_H
+#define __PERIPH_TIMER1_H
+
+/**
+ * @brief Configure timer 1
+ *
+ * This function does not start the timer.
+ *
+ * @param[in] prescaler
+ * @param[in] callback
+ */
+void timer1_configure(uint8_t prescaler, void(*callback)(void));
+
+/**
+ * @brief Start timer 1
+ */
+void timer1_start(void);
+
+/**
+ * @brief Stop timer 1
+ */
+void timer1_stop(void);
+
+#endif
